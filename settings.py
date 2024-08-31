bb4df85fcf3c2def812fa25253ad0c741f86b263
HopMaTuB <sunkiper@gmail.com>
2024-08-31 18:58:55 +0300

    update schemas,auth,email

--- settings.py
+++ settings.py
@@ -11,3 +11,19 @@ load_dotenv()
 SQLALCHEMY_DATABASE_URL = os.getenv('SQLALCHEMY_DATABASE_URL')
 SECRET_KEY = os.getenv('SECRET_KEY')
 ALGORITHM = os.getenv('ALGORITHM')
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/auth/login")
+
+conf = ConnectionConfig(
+    MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
+    MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
+    MAIL_FROM=os.getenv("MAIL_FROM"),
+    MAIL_PORT=os.getenv("MAIL_PORT"),
+    MAIL_SERVER=os.getenv("MAIL_SERVER"),
+    MAIL_FROM_NAME=os.getenv("MAIL_FROM_NAME"),
+    MAIL_STARTTLS=False,
+    MAIL_SSL_TLS=True,
+    USE_CREDENTIALS=True,
+    VALIDATE_CERTS=True,
+    TEMPLATE_FOLDER=Path(__file__).parent / 'templates',
+)
---
+++ src/repository/users.py
@@ -0,0 +1,85 @@
+from src.schemas import UserModel
+from src.repository.auth import Hash, create_access_token
+from sqlalchemy.orm import Session
+from fastapi.security import OAuth2PasswordRequestForm
+from src.configuration.models import User
+from typing import Optional,Union,Dict
+
+hash_handler = Hash()
+
+
+class UsernameToken(Exception):
+    """Exception raised when the username is already taken."""
+    pass
+
+class Wrongpassword(Exception):
+    """Exception raised when the password is incorrect."""
+    pass
+
+class LoginFailed(Exception):
+    """Exception raised when login fails."""
+    pass 
+
+
+class UserService:
+    """
+    Service class for handling user-related operations.
+    """
+
+    @staticmethod
+    def get_user(username: str, db: Session) -> Optional[User]:
+        user = db.query(User).filter(User.email == username).first()
+        return user
+
+
+    @staticmethod
+    def check_user_available(username: str, db: Session):
+        exist_user = UserService.get_user(username,db)
+        if exist_user:
+            raise UsernameToken
+        
+    @staticmethod
+    def create_new_user(body:UserModel, db: Session) -> Optional[Dict]:
+        UserService.check_user_available(username=body.email, db=db)
+        new_user = User(username=body.username,email=body.email,password=hash_handler.get_password_hash(body.password))
+        db.add(new_user)
+        db.commit()
+        db.refresh(new_user)
+        return new_user
+
+    
+    @staticmethod
+    def check_password(entered_password: str, database_password: str):
+        if not hash_handler.verify_password(entered_password, database_password):
+            raise Wrongpassword
+        
+    @staticmethod
+    def login_user(body: OAuth2PasswordRequestForm, db: Session):
+        user = UserService.get_user(body.username ,db = db)
+        if user is None or not hash_handler.verify_password(body.password, user.password):
+            raise LoginFailed
+        
+        access_token = create_access_token(data={"sub": user.email})
+        return access_token
+    
+    @staticmethod
+    def get_user_by_email(email: str, db: Session) -> User:
+        return db.query(User).filter(User.email == email).first()
+
+    @staticmethod
+    def confirmed_email(email: str, db: Session) -> None:
+        user = UserService.get_user_by_email(email, db)
+        user.confirmed = True
+        db.commit()
+
+    def update_token(user: User, token: Union[str, None], db: Session) -> None:
+        user.refresh_token = token
+        db.commit()
+
+    @staticmethod
+    def save_user(user_to_save: User, db: Session) -> User:
+        db.add(user_to_save)
+        db.commit()
+        db.refresh(user_to_save)
+        return user_to_save
+
